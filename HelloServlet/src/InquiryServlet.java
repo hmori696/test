bd8785f5fa49a40249aa0fbd29dfed4a76f57fb0
hmori696 <kikokijyo@yahoo.co.jp>
2019-09-18 20:57:37 +0900

    abd

--- HelloServlet/src/InquiryServlet.java
+++ HelloServlet/src/InquiryServlet.java
@@ -10,25 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-<<<<<<< HEAD
+
+
+
+
+
+
+
 @WebServlet("/InquiryServlet")
 public class InquiryServlet extends HttpServlet {
 
 
-=======
-
-
-@WebServlet("/InquiryServlet")
-public class InquiryServlet extends HttpServlet {
-
->>>>>>> 0194bd800cfe02c6a83fd1e5b25a2b8064c7502c
     public InquiryServlet() {
         super();
 
     }
 
 
-<<<<<<< HEAD
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -48,27 +47,8 @@ public class InquiryServlet extends HttpServlet {
 
 	}
 
-}
-=======
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
 
-	ServletException, IOException {
-
-		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html ; charset=UTF-8");
-	 String name = request.getParameter("name");
-
-
-	   System.out.println(name);
-
-
-	   PrintWriter out=response.getWriter();
-
-	  out.println("<html><head></head><body><br>"+name+"さん、お問い合わせありがとうございました</body></html>");}
 
 }
 
 
-
-
->>>>>>> 0194bd800cfe02c6a83fd1e5b25a2b8064c7502c
