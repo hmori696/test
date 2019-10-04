6510062e4a70d75c3e237beb7f3f77f119aaa160
hmori696 <kikokijyo@yahoo.co.jp>
2019-10-04 21:17:27 +0900

    today

--- HelloServlet/src/MySQLServlet.java
+++ HelloServlet/src/MySQLServlet.java
@@ -19,84 +19,79 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MySQLServlet")
 public class MySQLServlet extends HttpServlet {
 
+	public MySQLServlet() {
+		super();
 
-
-    public MySQLServlet() {
-        super();
-
-    }
+	}
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html); charset=UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
 
 		PrintWriter out = response.getWriter();
 
-out.println("<html>");
-out.println("<head>");
-out.println("<title>データベーステスト</title>");
-out.println("</head>");
-out.println("<body>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>データベーステスト</title>");
+		out.println("</head>");
+		out.println("<body>");
 
-Connection conn = null;
+		Connection conn = null;
 
-String url = "jdbc:mysql://localhost/testdb";
-String user = "root";
-String password ="mysql";
-
-
-try{
- Class.forName("com.mysql.jdbc.Driver").newInstance();
- conn = DriverManager.getConnection(url, user, password);
-
- Statement stmt = conn.createStatement();
- String sql = "SELECT * FROM test_table";
- ResultSet rs = stmt.executeQuery(sql);
+		String url = "jdbc:mysql://localhost/testdb";
+		String user = "root";
+		String password ="mysql";
 
 
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection(url, user, password);
 
-while(rs.next()){
-	int userID = rs.getInt("user_id");
-	String userName = rs.getString("user_name");
-	String userPassword =rs.getString("password");
-
-	out.println("<p>");
-    out.println("ユーザーID:" + userID + " , ユーザー名:" + userName + ", パスワード:" +
-userPassword);
-    out.println("</p>");
-
-}
-
-rs.close();
-stmt.close();
-
-}catch (ClassNotFoundException e){
-    out.println("ClassNotFoundException:" + e.getMessage()) ;
-}catch (SQLException e){
-	out.println("SQLException:" + e.getMessage());
-}catch(Exception e){
-	out.println("Exception:" + e.getMessage());
-}finally{
+			Statement stmt = conn.createStatement();
+			String sql = "SELECT * FROM test_table";
+			ResultSet rs = stmt.executeQuery(sql);
 
 
 
+			while(rs.next()){
+				int userID = rs.getInt("user_id");
+				String userName = rs.getString("user_name");
+				String userPassword =rs.getString("password");
 
-    try{
+				out.println("<p>");
+				out.println("ユーザーID:" + userID + " , ユーザー名:" + userName + ", パスワード:" +
+						userPassword);
+				out.println("</p>");
 
-    if (conn !=null){
-      conn.close();
-    }
+			}
+
+			rs.close();
+			stmt.close();
+
+		}catch (ClassNotFoundException e){
+			out.println("ClassNotFoundException:" + e.getMessage()) ;
+		}catch (SQLException e){
+			out.println("SQLException:" + e.getMessage());
+		}catch(Exception e){
+			out.println("Exception:" + e.getMessage());
+		}finally{
+
+			try{
+
+				if (conn !=null){
+					conn.close();
+				}
 
 
-}catch(SQLException e){
-     out.println("SQLException:" + e.getMessage());
+			}catch(SQLException e){
+				out.println("SQLException:" + e.getMessage());
 
-    }
-}
-    out.println("</body>");
-    out.println("</html>");
+			}
+		}
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
